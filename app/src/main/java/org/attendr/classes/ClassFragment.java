d4eccd6ebf444c2205a5859e23f00e1e8d3bcd94
Paul Gillis <pauldg7@gmail.com>
2017-11-26 19:22:17 -0600

    Fixed Swipe to delete View. OnClick needs to be implemented for deleting

--- app/src/main/java/org/attendr/classes/ClassFragment.java
+++ app/src/main/java/org/attendr/classes/ClassFragment.java
@@ -2,20 +2,21 @@ package org.attendr.classes;
 
 import android.content.res.Resources;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
-import android.util.DisplayMetrics;
-import android.util.Log;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.attendr.R;
@@ -25,13 +26,10 @@ import org.attendr.helpers.views.PercentageCircle;
 import java.util.ArrayList;
 import java.util.List;
 
-import static android.support.v7.widget.helper.ItemTouchHelper.UP;
-
 public class ClassFragment extends Fragment {
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View rootView = inflater.inflate(R.layout.fragment_classes, container, false);
         RecyclerView recyclerView = rootView.findViewById(R.id.recyclerView);
@@ -44,36 +42,80 @@ public class ClassFragment extends Fragment {
         details.setClassTitle("University Physics 1");
         details.setPercentage(55);
         cardAdapter.addClass(details);
-        ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
-            @Override
-            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
-                return false;
-            }
+        ClassDetails details2 = new ClassDetails();
+        details2.setProfessor("Joe Guilliams");
+        details2.setClassTitle("CS 2050");
+        details2.setPercentage(96);
+        cardAdapter.addClass(details2);
 
-            @Override
-            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
-                if (viewHolder instanceof CardHolder) {
-                    CardHolder holder = (CardHolder) viewHolder;
-                    if (direction == ItemTouchHelper.LEFT) {
-                        holder.moveToLeft();
-                    } else {
-                        holder.moveToRight();
-                    }
-
-                }
-            }
-
-            @Override
-            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
-                // view the background view
-            }
-        };
-
-// attaching the touch helper to recycler view
         new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(recyclerView);
 
         return rootView;
     }
+
+    private final ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new ItemTouchHelper.SimpleCallback(ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+
+        Drawable background;
+        Drawable trashDrawable;
+        boolean initiated;
+
+        private void init() {
+            background = new ColorDrawable(ContextCompat.getColor(getContext(), R.color.colorDeleteRed));
+            trashDrawable = ContextCompat.getDrawable(getContext(), R.drawable.trashcan);
+            initiated = true;
+        }
+
+        @Override
+        public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+
+        }
+
+        @Override
+        public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float originDX, float dY, int actionState, boolean isCurrentlyActive) {
+            View itemView = viewHolder.itemView;
+            if (viewHolder.getAdapterPosition() == -1) {
+                return;
+            }
+
+            if (!initiated) {
+                init();
+            }
+
+            int dX = (int) originDX;
+
+            int itemHeight = itemView.getBottom() - itemView.getTop();
+
+            if (-dX >= itemHeight) {
+                dX = -itemHeight;
+            } else if(dX > 0) {
+                dX = 0;
+            }
+
+            int backgroundLeft = itemView.getRight() + dX;
+            int backgroundRight = itemView.getRight();
+
+            background.setBounds(backgroundLeft, itemView.getTop(), backgroundRight, itemView.getBottom());
+            background.draw(c);
+
+            int intrinsicWidth = trashDrawable.getIntrinsicWidth();
+            int intrinsicHeight = trashDrawable.getIntrinsicWidth();
+
+            int canLeft = itemView.getRight() - (itemHeight/2) - (intrinsicWidth/2);
+            int canRight = itemView.getRight() - (itemHeight/2) + (intrinsicWidth/2);
+            int canTop = itemView.getTop() + (itemHeight - intrinsicHeight)/2;
+            int canBottom = canTop + intrinsicHeight;
+            trashDrawable.setBounds(canLeft, canTop, canRight, canBottom);
+
+            trashDrawable.draw(c);
+
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+    };
 
     class CardAdapter extends RecyclerView.Adapter<CardHolder> {
 
@@ -112,38 +154,13 @@ public class ClassFragment extends Fragment {
 
     class CardHolder extends RecyclerView.ViewHolder {
 
-        private RelativeLayout foreground;
-
         private PercentageCircle percentageWheel;
         private TextView classTitle;
 
-        private boolean leftMoved = false;
-        private boolean rightMoved = false;
-
-        private int dp;
-
         CardHolder(View itemView) {
             super(itemView);
-            foreground = itemView.findViewById(R.id.foreground);
             percentageWheel = itemView.findViewById(R.id.percentageWheel);
             classTitle = itemView.findViewById(R.id.classTitle);
-            dp = (int) (100 * Resources.getSystem().getDisplayMetrics().density);
-        }
-
-        void moveToLeft() {
-            if (!leftMoved) {
-                leftMoved = true;
-                rightMoved = false;
-                foreground.offsetLeftAndRight(-dp);
-            }
-        }
-
-        void moveToRight() {
-            if (!rightMoved) {
-                leftMoved = false;
-                rightMoved = true;
-                foreground.offsetLeftAndRight(dp);
-            }
         }
 
         void setData(ClassDetails details) {
